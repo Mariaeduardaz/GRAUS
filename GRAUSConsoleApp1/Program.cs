cc4e6ef1cefe694dbbdd66dbb29fa493fb9e27f0
Mariaeduardaz <vitorvogel12@gmail.com>
2022-02-05 00:46:34 -0300

    Add project files.

---
+++ GRAUSConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GRAUSConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("graus");
+            Console.WriteLine("Quantos graus Celsius voce quer transformar em Fahrenheit?");
+            double c = double.Parse(Console.ReadLine());
+            double f = c * 1.8 + 32;
+            Console.Clear();
+            Console.WriteLine($"{c} graus Celsius é equivalente a {f}");
+            Console.ReadLine();
+        }
+    }
+}
